0abd2bcea88e8e21f1c458772c1f0ef8b72da40e
bala-amypo <bala@amypo.in>
2025-12-18 08:37:25 +0000

    User commit

--- src/main/java/com/example/demo/service/ExamRoomService.java
+++ src/main/java/com/example/demo/service/ExamRoomService.java
@@ -1,3 +1,35 @@
-public class ExamRoomService{
-    
+// public class ExamRoomService{
+
+// }
+package com.example.demo.service;
+
+import com.example.demo.exception.ApiException;
+import com.example.demo.model.ExamRoom;
+import com.example.demo.repository.ExamRoomRepository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExamRoomService {
+
+    private final ExamRoomRepository examRoomRepository;
+
+    public ExamRoomService(ExamRoomRepository examRoomRepository) {
+        this.examRoomRepository = examRoomRepository;
+    }
+
+    public ExamRoom addRoom(ExamRoom room) {
+
+        if (examRoomRepository.findByRoomNumber(room.getRoomNumber()) != null) {
+            throw new ApiException("exists");
+        }
+
+        return examRoomRepository.save(room);
+    }
+
+    public List<ExamRoom> getAllRooms() {
+        return examRoomRepository.findAll();
+    }
 }
