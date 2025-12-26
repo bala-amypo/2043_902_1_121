47ae943fc1cd79753fb12320b28eb2c0bec55e9b
bala-amypo <bala@amypo.in>
2025-12-26 15:01:16 +0000

    User commit

--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -7,6 +7,6 @@ import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
-    // ✅ Tests expect Optional
+    //  Tests expect Optional
     Optional<Student> findByRollNumber(String rollNumber);
 }
